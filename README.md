741088ebbef5db20ded69bd9d930a54ffb743086
valevil <swamptk@gmail.com>
2025-05-31 12:14:08 +0200

    Batman

---
+++ README.md
@@ -0,0 +1,12 @@
+# NBA Most Valuable Player Votation Analysis
+
+Following Shai Gilgeous-Alexander's selection as the 2024-2025 regular season MVP over Serbian center Nikola Jokić,  
+many fans expressed disagreement with the decision on social media.
+
+This analysis examines whether the award adhered to the same criteria as in previous years.  
+We compiled data for the top 10 MVP vote-getters each season from 1980-1981 onward.
+
+> We chose this season as our starting point because it introduced the three-point field goal (3FG)  
+> and established the framework for _modern basketball_.
+
+- Data from [Basketball Reference](https://www.basketball-reference.com/)
---
+++ get_data.py
@@ -0,0 +1,27 @@
+import random
+import requests
+from pathlib import Path
+from time import sleep
+import pandas as pd
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/123.0 Safari/537.36",
+    "Referer": "https://www.google.com",
+    "Accept-Language": "es-ES,es;q=0.9",
+}
+
+proxies = {
+    "http": "socks5h://127.0.0.1:9050",
+    "https": "socks5h://127.0.0.1:9050",
+}
+
+data_path = Path("data")
+for year in range(1980, 2024):
+    filepath = data_path / f"mvp_{year}.csv"
+    url = f"https://www.basketball-reference.com/awards/awards_{year}.html"
+    response = requests.get(url, headers=headers, proxies=proxies)
+    assert response.status_code == 200, f"Error - { response.status_code = }"
+    df = pd.read_html(response.text, header=1)[0].head(10)
+    df["Year"] = year
+    df.to_csv(filepath, index=False)
+    sleep(random.uniform(3, 7))
